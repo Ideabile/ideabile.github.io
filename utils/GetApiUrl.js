0fde2ab4ef40bbfd85ed4224787dddcebfbb62ff
m <m@ms-MacBook-Pro.local>
2017-07-23 19:44:42 +0200

    Fix get url

--- utils/GetApiUrl.js
+++ utils/GetApiUrl.js
@@ -1,7 +1,7 @@
 
 export default function GetApiUrl (env, isClient, path) {
-  let base = !env.generated ? env.apiUrl : `${env.baseUrl}api/`
-  let suffix = isClient && env.generated ? '.json' : ''
-  let fullUrl = `${base}${path}${suffix}`
-  return fullUrl
+  if( isClient && env.generated ){
+    return `${env.baseUrl}api/${path}.json`
+  }
+  return `${env.apiUrl}${path}`
 }
